ac459182720745b4ef0be2258c58c9416fde59c7
indreshgahoi <indreshgahoi@gmail.com>
2016-07-08 18:51:27 +0530

    adding server client model

--- src/genericutil/ReflectionExample.java
+++ src/genericutil/ReflectionExample.java
@@ -1,4 +1,4 @@
-package generic;
+package genericutil;
 
 import java.lang.reflect.Type;
 import java.util.Map;
--- src/utility/thread/server.java
+++ src/utility/thread/server.java
@@ -11,13 +11,40 @@ class server
 		ServerSocket s=new ServerSocket(8009);
 		while(true)
 		{
-			Socket s1=s.accept();
-			OutputStream out=s1.getOutputStream();
+			Socket clientConn = s.accept();
+			
+			OutputStream out = clientConn.getOutputStream();
+			InputStream  in = clientConn.getInputStream(); 
 			BufferedWriter bw=new BufferedWriter(new OutputStreamWriter(out));
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
 			bw.write("Hello Students\n");
 			bw.close();
-			s1.close();
+			clientConn.close();
 		}
 		}catch(Exception e){}
 	}
+}
+
+
+class Handler{
+	
+	public Handler(Socket conn, BufferedReader br, BufferedWriter bw){
+		
+	}
+	
+	
+	
+}
+
+class Reader implements Runnable {
+	
+	
+	public Reader(Handler handler){
+		
+	}
+	
+		public void run() {
+			
+		}
+	
 }
